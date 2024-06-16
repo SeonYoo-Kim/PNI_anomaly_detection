2d8f864ceef898c968948bed6a6e365d67afcd15
theta <tjdn13@naver.com>
2024-06-16 13:52:48 +0900

    all

--- evaluate_amap_on_MVTecAD_WR101.sh
+++ evaluate_amap_on_MVTecAD_WR101.sh
@@ -3,39 +3,39 @@
 # Note that dataset should be in directory of "--dataset_path" argument. 
 # If you want other pretrained network rather than WideResNet101-2, change "--backbone" argument.
 
-python train_coreset_distribution.py --category bottle --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
-python train_coreset_distribution.py --category cable --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
-python train_coreset_distribution.py --category capsule --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
-python train_coreset_distribution.py --category carpet --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
-python train_coreset_distribution.py --category grid --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
-python train_coreset_distribution.py --category hazelnut --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
-python train_coreset_distribution.py --category leather --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
-python train_coreset_distribution.py --category metal_nut --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
-python train_coreset_distribution.py --category pill --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
-python train_coreset_distribution.py --category screw --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
-python train_coreset_distribution.py --category tile --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
-python train_coreset_distribution.py --category toothbrush --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
-python train_coreset_distribution.py --category transistor --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
-python train_coreset_distribution.py --category wood --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
-python train_coreset_distribution.py --category zipper --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
+python train_coreset_distribution.py --category bottle --seed 23 --dataset_path ../dataset/MVTecAD
+#python train_coreset_distribution.py --category cable --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
+#python train_coreset_distribution.py --category capsule --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
+#python train_coreset_distribution.py --category carpet --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
+#python train_coreset_distribution.py --category grid --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
+#python train_coreset_distribution.py --category hazelnut --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
+#python train_coreset_distribution.py --category leather --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
+#python train_coreset_distribution.py --category metal_nut --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
+#python train_coreset_distribution.py --category pill --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
+#python train_coreset_distribution.py --category screw --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
+#python train_coreset_distribution.py --category tile --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
+#python train_coreset_distribution.py --category toothbrush --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
+#python train_coreset_distribution.py --category transistor --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
+#python train_coreset_distribution.py --category wood --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
+#python train_coreset_distribution.py --category zipper --seed 23 --train_coreset --train_nb_dist --train_coor_dist --dataset_path ../dataset/MVTecAD
 
 # make ensemble score for each category and save the result in "./result/ensemble_result" repository.
 #"--backbone_list" argument is list of pretrained networks which are to ensemble. You can change category with "--category" path. 
 python analysis_code/calc_ensemble_score.py --category bottle --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
-python analysis_code/calc_ensemble_score.py --category cable --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
-python analysis_code/calc_ensemble_score.py --category capsule --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
-python analysis_code/calc_ensemble_score.py --category carpet --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
-python analysis_code/calc_ensemble_score.py --category grid --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
-python analysis_code/calc_ensemble_score.py --category hazelnut --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
-python analysis_code/calc_ensemble_score.py --category leather --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
-python analysis_code/calc_ensemble_score.py --category metal_nut --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
-python analysis_code/calc_ensemble_score.py --category pill --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
-python analysis_code/calc_ensemble_score.py --category screw --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
-python analysis_code/calc_ensemble_score.py --category tile --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
-python analysis_code/calc_ensemble_score.py --category toothbrush --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
-python analysis_code/calc_ensemble_score.py --category transistor --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
-python analysis_code/calc_ensemble_score.py --category wood --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
-python analysis_code/calc_ensemble_score.py --category zipper --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
+#python analysis_code/calc_ensemble_score.py --category cable --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
+#python analysis_code/calc_ensemble_score.py --category capsule --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
+#python analysis_code/calc_ensemble_score.py --category carpet --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
+#python analysis_code/calc_ensemble_score.py --category grid --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
+#python analysis_code/calc_ensemble_score.py --category hazelnut --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
+#python analysis_code/calc_ensemble_score.py --category leather --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
+#python analysis_code/calc_ensemble_score.py --category metal_nut --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
+#python analysis_code/calc_ensemble_score.py --category pill --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
+#python analysis_code/calc_ensemble_score.py --category screw --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
+#python analysis_code/calc_ensemble_score.py --category tile --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
+#python analysis_code/calc_ensemble_score.py --category toothbrush --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
+#python analysis_code/calc_ensemble_score.py --category transistor --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
+#python analysis_code/calc_ensemble_score.py --category wood --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
+#python analysis_code/calc_ensemble_score.py --category zipper --backbone_list WR101 --project_root_path ./result --ensemble_root_path ./result/WR101_result
 
 # convert result format and save it into "./result/ensemble_ravel" repository.
 # Add argument "--is_BTAD" if dataset is BTAD, and "--is_MVtec_small" if dataset is small version of MVTec which we provided.
