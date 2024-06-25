e13cfebed61b1f9f2c4c8debcffb9ac8a6a28b83
theta <tjdn13@naver.com>
2024-06-25 16:27:50 +0900

    all

---
+++ evaluate_amap_on_AeBAD_refinement.sh
@@ -0,0 +1,6 @@
+# refine anomaly map which format is ensemble_ravel, using trained refinement network.
+# You can change category with "--category" path. 
+# calculated anomaly map before refinement should be in directory of "--input_data_path" argument.
+# Refined anomaly map will be saved in directory of "--output_data_path" argument.
+
+python pixelwise_refinement.py --input_data_path result_AeBAD/WR101_ravel --output_data_path result_AeBAD/refined_ravel
